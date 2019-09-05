e9509ac21bfe5e1bbb54d8813ccd90dd41ef32fa
Mikael Ingi <mikael.ingi@gmail.com>
2019-09-05 14:24:34 +0000

    Changed the input string in the while loop.

--- max_int.py
+++ max_int.py
@@ -5,5 +5,5 @@ max_int=0
 while num_int>0:
     if num_int>max_int:
         max_int=num_int
-    num_int = int(input('Input another number: '))
+    num_int = int(input('Input a number: '))
 print("The maximum is", max_int)    # Do not change this line

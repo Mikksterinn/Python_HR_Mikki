2edef7dee05adbb458903b1c6694ad568365d43e
Mikael Ingi <mikael.ingi@gmail.com>
2019-09-15 17:04:58 +0000

    Moved the FIELD_RANGE constant to the first line and used it throughout the program.

--- Heimaverkefni/move.py
+++ Heimaverkefni/move.py
@@ -1,16 +1,18 @@
-# The function used to create the string of x's and the singular "o".
+FIELD_RANGE = 10 # The maximum range of the string of "x's" and the singular "o".
+
+# The function used to create the string of "x's" and the singular "o".
 def position(o_coordinate): # Defining the function name and the input variable.
-    FIELD_RANGE = 10 # The maximum range of the string.
     playing_field = '' # Starting off with an empty string.
-    for i in range(1, FIELD_RANGE+1): # The for loop adds x's to the empty string for every number other than the o_coordinate.
+    for i in range(1, FIELD_RANGE+1): # The for loop adds "x's" to the empty string for every number other than the o_coordinate.
         if i == o_coordinate:
             playing_field += 'o'
         else:
             playing_field += 'x'
-    return playing_field # The function returns the constructed string of x's and the "o" where the "o" is in the position of the o_coordinate.
+    return playing_field # The function returns the constructed string of "x's" and the "o" where the "o" is in the position of the o_coordinate.
 # End of function.
 
-coordinate = int(input('Input a position between 1 and 10: ')) # The user input for the initial o_coordinate.
+initial_question = 'Input a position between 1 and ' + str(FIELD_RANGE) + ': ' # Constucting the first question the user is asked using the FIELD_RANGE constant.
+coordinate = int(input(initial_question)) # The user input for the initial o_coordinate.
 
 starting_pos = position(coordinate) # Sending the initial o_coordinate to the position function.
 print(starting_pos) # Printing the initial string.
@@ -22,7 +24,7 @@ print('Any other letter for quitting')
 movement = input('Input your choice: ') # The variable used in the while loop to move the "o" either to the right or left.
 
 while movement == 'l' or movement == 'r':
-    if movement == 'r' and coordinate < 10: # The "and" statement is there to make sure the "o" doesn't go outside the range of the string.
+    if movement == 'r' and coordinate < FIELD_RANGE: # The "and" statement is there to make sure the "o" doesn't go outside the range of the string.
         coordinate += 1 # if the user chooses "r" then the o_coordinate gets 1 added to it and thus the "o" shifts to the right when the string is constructed.
     elif movement == 'l' and coordinate > 1: # The "and" statement is there to make sure the "o" doesn't go outside the range of the string.
         coordinate -= 1 # if the user chooses "l" then the o_coordinate gets 1 subtracted from it and thus the "o" shifts to the left when the string is constructed.
